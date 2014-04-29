5b9c6277920d67111a404fdb8ee9bb4b4699825c
Imran Ahmed <imran.nappa@gmail.com>
2014-04-29 10:30:24 +0600

    Update plugin.rb

--- crichq_elasticsearch/recipes/plugin.rb
+++ crichq_elasticsearch/recipes/plugin.rb
@@ -1,15 +1,13 @@
-script "install_plugin_es_head" do
+script "install_plugins" do
 	interpreter "bash"
 	user "root"
 	cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
 	code <<-EOH
-	  ## site plugins
-  	plugin -install mobz/elasticsearch-head
+	  ## site plugins 
   	plugin -install royrusso/elasticsearch-HQ
   	plugin -install lukas-vlcek/bigdesk
-    plugin -install karmi/elasticsearch-paramedic
+        plugin -install karmi/elasticsearch-paramedic
   	
   	EOH
-	not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/head") }
-	ignore_failure true
+ 	ignore_failure true
 end
